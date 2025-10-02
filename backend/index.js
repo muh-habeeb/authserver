66556e22fd1304a5cbe5707a486cf64cd3479d53
habeeb <muhhabeeb787@gmail.com>
2025-10-02 17:53:05 +0530

    server in 5000 and front end also

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { connectDB } from "./db/connectDb.js";
 import dotenv from "dotenv";
-import authRoutes from "../backend/routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 import path from "path"
@@ -9,20 +9,28 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:3000",
-  credentials:true,
-  methods:["GET","POST"]
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST"]
 }))
 
-const __dirname=path.resolve() ;
+const __dirname = path.resolve();
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
 connectDB();
+
+
 app.use("/api/auth", authRoutes);
 
-
-
-app.listen(PORT, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  
+  // Serve React app for any non-API routes
+  app.get(/^(?!\/api).*$/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
+  app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
